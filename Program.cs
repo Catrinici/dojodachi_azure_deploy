b4f8d6c702c622cad9fa1f7d90b7d10de6041c30
Catrinici <catrinici.antonina@gmail.com>
2017-04-12 12:08:20 -0700

    dojodachi deployment

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Dojodachi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseIISIntegration()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
